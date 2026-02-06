6b3b7eb77102a1129752c9982eff275667dab357
Keith Mendoza <576988+keithmendozasr@users.noreply.github.com>
2026-02-06 11:08:33 -0800

    feat: Add ResourceStatusTool

---
+++ src/Tools/ResourceStatusTool.cs
@@ -0,0 +1,35 @@
+using McpServer.Models;
+
+namespace McpServer.Tools;
+
+public class ResourceStatusTool
+{
+    public object ListTools()
+    {
+        return new
+        {
+            tools = new[]
+            {
+                new { name = "getResourceStatus", description = "Returns status of resources" }
+            }
+        };
+    }
+
+    public Task<object> GetResourceStatusAsync(CancellationToken ct)
+    {
+        // Hardcoded mock resources
+        var resources = new List<ResourceDto>
+        {
+            new ResourceDto { Name = "res-1", Type = "generic", Status = "ready" },
+            new ResourceDto { Name = "res-2", Type = "generic", Status = "active" }
+        };
+
+        var result = new
+        {
+            count = resources.Count,
+            resources
+        };
+
+        return Task.FromResult<object>(result);
+    }
+}
